8a81535eed6d2161b74e43254c387578b3df7636
Amirmehdi Sharifzad <amsharifzad@gmail.com>
2018-03-01 19:53:50 -0500

    Initial Commit

---
+++ OpenCVProjectTests/OpenCVProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  OpenCVProjectTests.swift
+//  OpenCVProjectTests
+//
+//  Created by Amirmehdi Sharifzad on 2018-03-01.
+//  Copyright © 2018 Hack The Valley II. All rights reserved.
+//
+
+import XCTest
+@testable import OpenCVProject
+
+class OpenCVProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
