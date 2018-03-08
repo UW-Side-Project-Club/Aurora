3dc1ca5fa23776ea2e2312103975f89e02001f7f
Amirmehdi Sharifzad <amsharifzad@gmail.com>
2018-03-07 20:37:25 -0500

    first commit

---
+++ OpenCVProject/FrameExtractor.swift
@@ -0,0 +1,153 @@
+//
+//  FrameExtractor.swift
+//  OpenCVProject
+//
+//  Created by Amirmehdi Sharifzad on 2018-03-01.
+//  Copyright © 2018 Hack The Valley II. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+protocol FrameExtractorDelegate: class {
+    func captured(image: UIImage)
+    func observed(results: [VNClassificationObservation])
+}
+
+class FrameExtractor: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    private let position = AVCaptureDevice.Position.back
+    private let quality = AVCaptureSession.Preset.high
+    
+    private var permissionGranted = false
+    private let sessionQueue = DispatchQueue(label: "session queue")
+    private let captureSession = AVCaptureSession()
+    private let context = CIContext()
+    
+    weak var delegate: FrameExtractorDelegate?
+    
+    override init() {
+        print("initializing frameExtractor")
+        super.init()
+        checkPermission()
+        sessionQueue.async { [unowned self] in
+            self.configureSession()
+            self.captureSession.startRunning()
+        }
+    }
+    
+    // MARK: AVSession configuration
+    private func checkPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: AVMediaType.video) {
+        case .authorized:
+            permissionGranted = true
+        case .notDetermined:
+            requestPermission()
+        default:
+            permissionGranted = false
+        }
+    }
+    
+    private func requestPermission() {
+        sessionQueue.suspend()
+        AVCaptureDevice.requestAccess(for: AVMediaType.video) { [unowned self] granted in
+            self.permissionGranted = granted
+            self.sessionQueue.resume()
+        }
+    }
+    
+    private func configureSession() {
+        print("configuring")
+        guard permissionGranted else { return }
+        captureSession.sessionPreset = quality
+        guard let captureDevice = selectCaptureDevice() else { return }
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        guard captureSession.canAddInput(captureDeviceInput) else { return }
+        captureSession.addInput(captureDeviceInput)
+        let videoOutput = AVCaptureVideoDataOutput()
+    
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sample buffer"))
+        //self.configureDevice(captureDevice: captureDevice)
+        guard captureSession.canAddOutput(videoOutput) else { return }
+        captureSession.addOutput(videoOutput)
+        guard let connection = videoOutput.connection(with: AVFoundation.AVMediaType.video) else { return }
+        guard connection.isVideoOrientationSupported else { return }
+        guard connection.isVideoMirroringSupported else { return }
+        connection.videoOrientation = .portrait
+        connection.isVideoMirrored = position == .front
+    }
+    
+    private func configureDevice(captureDevice: AVCaptureDevice?) {
+        if let device = captureDevice {
+            print("doing1")
+            
+            // 1
+            for vFormat in captureDevice!.formats {
+                
+                // 2
+                var ranges = vFormat.videoSupportedFrameRateRanges as [AVFrameRateRange]
+                let frameRates = ranges[0]
+                print(frameRates.maxFrameRate)
+                // 3
+                if frameRates.maxFrameRate == 30 {
+                    
+                    // 4
+                    do {
+                        print("doing")
+                        try device.lockForConfiguration()
+                        device.activeFormat = vFormat as AVCaptureDevice.Format
+                        device.activeVideoMinFrameDuration = frameRates.minFrameDuration
+                        device.activeVideoMaxFrameDuration = frameRates.maxFrameDuration
+                        device.unlockForConfiguration()
+                    } catch {
+                    
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+    
+    private func selectCaptureDevice() -> AVCaptureDevice? {
+        // search for available capture devices
+        return AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: position).devices.first
+    }
+    
+    // MARK: Sample buffer to UIImage conversion
+    private func imageFromSampleBuffer(sampleBuffer: CMSampleBuffer) -> UIImage? {
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return nil }
+        //print(imageBuffer)
+        let ciImage = CIImage(cvPixelBuffer: imageBuffer)
+        //print(ciImage)
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return nil }
+        return UIImage(cgImage: cgImage)
+    }
+    
+    // MARK: AVCaptureVideoDataOutputSampleBufferDelegate
+    func captureOutput(_ captureOutput: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        //UI Image
+        guard let uiImage = imageFromSampleBuffer(sampleBuffer: sampleBuffer) else { return }
+        //Classification model
+        //guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { return }
+        /* classification request
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+            guard let results = finishedRequest.results as? [VNClassificationObservation]
+                else { return }
+            DispatchQueue.main.async(execute: { [unowned self] in
+                self.delegate?.observed(results: results)
+            })
+        } */
+           
+        DispatchQueue.main.async { [unowned self] in
+            self.delegate?.captured(image: uiImage)
+        }
+
+       // guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        // executes request
+        //try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+}
+
--- OpenCVProject/ViewController.swift
+++ OpenCVProject/ViewController.swift
@@ -7,19 +7,150 @@
 //
 
 import UIKit
+import Speech
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FrameExtractorDelegate {
+   
+    var frameExtractor : FrameExtractor!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    
+    var speechResult : String!
+    var imageData: Data!
+    var node: AVAudioInputNode!
+    var request : SFSpeechAudioBufferRecognitionRequest!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        frameExtractor = FrameExtractor()
+        frameExtractor.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func capPressed(_ sender: Any) {
+        postRequest(url: "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/describe", image: imageView.image!)
+    }
+    func captured(image: UIImage) {
+        imageView.image = image
+      /*  let delay = DispatchTime.now() + .seconds(10)
+        DispatchQueue.main.asyncAfter(deadline: delay) {
+            print("delayed")
+        } */
+    }
+    
+    func observed(results: [VNClassificationObservation]) {
+        // access the AVSpeechSynthesizer Class
+        let speakTalk = AVSpeechSynthesizer()
+        let observation = results.first
+        if (observation?.confidence)! > 0.5 {
+            let talk = AVSpeechUtterance(string: (observation?.identifier)!)
+            speakTalk.speak(talk)
+        }
+    }
+    
+    func postRequest(url: String, image: UIImage) {
+        var request = URLRequest(url: URL(string: url)!)
+        let session = URLSession.shared
+        request.httpMethod = "POST"
+        request.httpBody = UIImageJPEGRepresentation(image, 1)
+        request.addValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
+        request.addValue("5b12f0c970b0483896897cbfaa8672b1", forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
+        
+        let task = session.dataTask(with: request, completionHandler: {data, response, error -> Void in
+            print("Response: \(String(describing: response))")
+            let strData = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print("Body: \(String(describing: strData))")
+            // parse the result as JSON
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(APIResponse.self, from: data!)
+                self.speak(text: "I see "+response.description.captions[0].text)
+                
+                
+            } catch {
+                print("error parsing data")
+                return
+            }
+        })
+        
+        task.resume()
+    }
+    
+    func speak(text: String) {
+        let synth = AVSpeechSynthesizer()
+        let speech = AVSpeechUtterance(string: text )
+        synth.speak(speech)
+    }
+    
+    func requestSpeechAuth() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        } catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
+        self.request = SFSpeechAudioBufferRecognitionRequest()
+        node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            self.request.append(buffer)
+        }
+        print("else22")
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        
+        print("else24")
+        
+        let recognizer = SFSpeechRecognizer()
+        recognizer?.recognitionTask(with: self.request) { (results, error) in
+            
+            
+            if let error = error {
+                print("There was an error: \(error)")
+            } else {
+                //print(results?.bestTranscription.formattedString as Any)
+                //print(results?.isFinal as Any)
+                self.speechResult = results?.bestTranscription.formattedString
+            }
+        }
+    }
+    
 
+    
+}
 
+struct APIResponse : Codable {
+    struct MetaData : Codable {
+        let height: Int
+        let width: Int
+        let format: String
+    }
+    struct Description : Codable {
+        struct Captions : Codable {
+            let text: String
+            let confidence: Float
+        }
+        let tags: [String]
+        let captions: [Captions]
+    }
+    
+    let description: Description
+    let requestId: String
+    let metadata: MetaData
 }
 
